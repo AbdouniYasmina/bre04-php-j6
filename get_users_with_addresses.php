54244a86ddc2723185f500830970c184777926f0
AbdouniYasmina <abdouniyasmina7@gmail.com>
2025-06-11 12:03:13 +0000

    étape 5 : get users with addresses

---
+++ get_users_with_addresses.php
@@ -0,0 +1,12 @@
+<?php
+
+require 'connexion.php';
+
+$query = $db->prepare('SELECT users.first_name, users.last_name, address.* 
+FROM users JOIN address ON users.address = address.id');
+$query->execute();
+$users = $query->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($users)
+
+?>
